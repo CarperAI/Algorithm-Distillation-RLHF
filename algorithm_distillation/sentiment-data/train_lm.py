b983670f857b6fe8c452f05daab70f127d4c7da0
Thomas Foster <foster@genei.io>
2023-01-06 15:34:52 +0000

    add eval loop

--- algorithm_distillation/sentiment-data/train_lm.py
+++ algorithm_distillation/sentiment-data/train_lm.py
@@ -8,24 +8,43 @@ from tqdm.auto import tqdm
 accelerator = Accelerator()
 
 model = AutoModelForCausalLM.from_pretrained('gpt2')
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
 
 tokenizer = AutoTokenizer.from_pretrained('gpt2')
-dataset = RolloutsAsLanguageModellingTask(tokenizer, './decoded_rollouts')
-data = torch.utils.data.DataLoader(dataset, shuffle=False)
+train_dataset = RolloutsAsLanguageModellingTask(tokenizer, './decoded_rollouts/train')
+eval_dataset = RolloutsAsLanguageModellingTask(tokenizer, './decoded_rollouts/eval')
 
-model, optimizer, data = accelerator.prepare(model, optimizer, data)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, shuffle=False)
+
+model, optimizer, train_dataloader = accelerator.prepare(model, optimizer, train_dataloader)
 
 model.train()
+total_steps = 0
 for epoch in range(10):
-    for batch in tqdm(data, desc=f'Epoch {epoch}'):
-
+    for batch in tqdm(train_dataloader, desc=f'Training epoch {epoch}'):
+        # train
         optimizer.zero_grad()
-
         output = model(**batch)
-        
         loss = output.loss
-
         accelerator.backward(loss)
-
         optimizer.step()
+        total_steps += 1
+        
+        # eval
+        eval_steps = 25
+        eval_size = 20
+        if total_steps % eval_steps == 0:
+            model.eval()
+            eval_loss = 0
+            eval_dataloader = torch.utils.data.DataLoader(eval_dataset, shuffle=False)
+            eval_dataloader = accelerator.prepare(eval_dataloader)
+            for idx, batch in tqdm(enumerate(eval_dataloader), desc=f'Eval after {total_steps} steps.'):
+                if idx >= eval_size:
+                    break
+                output = model(**batch)
+                loss = output.loss
+                eval_loss += loss.item()
+            eval_loss /= idx
+            print(f'Avg loss after {total_steps}: {eval_loss}')
+            model.train()
+                
